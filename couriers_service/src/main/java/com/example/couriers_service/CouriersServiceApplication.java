f574f895e3b434caeb2ee0b90c9af5a1cc77c0fb
arthur <arthur.kentbayev@gmail.com>
2021-10-03 23:45:35 +0600

    eureka

--- couriers_service/src/main/java/com/example/couriers_service/CouriersServiceApplication.java
+++ couriers_service/src/main/java/com/example/couriers_service/CouriersServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.couriers_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class CouriersServiceApplication {
 
   public static void main(String[] args) {
