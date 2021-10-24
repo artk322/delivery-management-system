e99a3c0a5ff33bdbded476fcfe7bab0989999c98
arthur <arthur.kentbayev@gmail.com>
2021-10-24 22:28:22 +0600

    Swagger

--- orders_service/src/main/java/com/example/orders_service/config/SwaggerConfig.java
+++ orders_service/src/main/java/com/example/orders_service/config/SwaggerConfig.java
@@ -1,2 +1,28 @@
-package com.example.orders_service.config;public class SwaggerConfig {
+package com.example.orders_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+  @Bean
+  public Docket postsApi() {
+    return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+        .apiInfo(apiInfo()).select().build();
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder().title("Orders API")
+        .description("Orders API")
+        .termsOfServiceUrl("https://github.com/artk322/delivery-management-system")
+        .version("1.0").build();
+  }
 }
+
