26f73900e3d49377d7a356585f0b6643f0375778
DimonCool00 <smirdimon2000@mail.ru>
2021-12-20 01:56:48 +0600

    products

--- products-service/src/main/java/com/example/productsservice/controller/ProductController.java
+++ products-service/src/main/java/com/example/productsservice/controller/ProductController.java
@@ -1,4 +1,23 @@
 package com.example.productsservice.controller;
 
+import com.example.productsservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List> getAllProducts(){
+        return ResponseEntity.ok(productService.get_all_products());
+    }
+
+
 }
--- products-service/src/main/java/com/example/productsservice/service/impl/ProductServiceImpl.java
+++ products-service/src/main/java/com/example/productsservice/service/impl/ProductServiceImpl.java
@@ -2,16 +2,28 @@ package com.example.productsservice.service.impl;
 
 import com.example.productsservice.model.Product;
 import com.example.productsservice.service.ProductService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService {
   private List<Product> products = Arrays.asList(
-      new Product(1L, "Burger", 500),
-      new Product(2L, "Burger", 500),
-      new Product(3L, "Burger", 500)
+      new Product(1L, "Hamburger", 700),
+      new Product(2L, "CheeseBurger", 800),
+      new Product(3L, "French fries", 400),
+      new Product(4L, "Hot-Dog", 750),
+      new Product(5L, "Coca-Cola", 200),
+      new Product(6L, "FuseTea", 200),
+          new Product(7L, "Coffee", 400),
+      new Product(8L, "Tea", 250),
+      new Product(9L, "Chicken strips", 800),
+      new Product(10L, "Chicken wings", 900),
+      new Product(11L, "Nuggets", 750),
+      new Product(12L, "Chicken popcorn", 550),
+      new Product(13L, "Ice cream", 300)
   );
 
   @Override
